e0bcf927c4555877160300fa01c5525d3af7741f
Benhadid Adnane <adnane.benhadid@gmail.com>
2019-01-12 13:27:15 +0100

    fichier de démarrage pour le devoir1

---
+++ devoir1/automate.s
@@ -0,0 +1,46 @@
+# automate.s ... on a 10 x 10 grid
+
+      .data
+
+N:    .word 10  # nombre de lignes de la grille
+M:    .word 10  # nombre de colonnes de la grille
+
+iter: .word 4   # nombre d'iterations
+
+grid:           # grille de cellules
+      .byte 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      .byte 1, 1, 0, 0, 0, 0, 0, 0, 0, 0
+      .byte 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+      .byte 0, 0, 1, 0, 1, 0, 0, 0, 0, 0
+      .byte 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+      .byte 0, 0, 0, 0, 1, 1, 1, 0, 0, 0
+      .byte 0, 0, 0, 1, 0, 0, 1, 0, 0, 0
+      .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+      .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+      .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+
+othergrid: .space 100
+
+      .text
+      .globl main
+
+### Votre fonction principale main doit être implémentée entre    ### 
+### les marqueurs VOTRE_CODE_COMMENCE_ICI et VOTRE_CODE_FINI_ICI  ###
+
+main:
+# VOTRE_CODE_COMMENCE_ICI
+
+
+# VOTRE_CODE_FINI_ICI
+   
+   li $v0, 10       # exit code service 
+   syscall          # for syscall
+
+### Vos éventuelles fonctions doivent être implémentées en dessous !    ###
+### entre les marqueurs VOTRE_CODE_COMMENCE_ICI et VOTRE_CODE_FINI_ICI  ###
+
+# VOTRE_CODE_COMMENCE_ICI
+
+# VOTRE_CODE_FINI_ICI
+
+
